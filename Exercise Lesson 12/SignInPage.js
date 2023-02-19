eef473d9fe77b4f5e24f59207ccf86eea1a01893
Sang-Nguyen-Phuoc <nguyenphuocsang04012004@gmail.com>
2023-02-19 19:29:54 +0700

    Exercise Lesson 12

--- Exercise Lesson 12/SignInPage.js
+++ Exercise Lesson 12/SignInPage.js
@@ -1,11 +1,12 @@
-const form = document.getElementById("sign-up"); 
+const form = document.getElementById("sign-in"); 
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const user = JSON.parse(localStorage.getItem("user")); 
-    if ((user.email  === event.target.email.value) && (user.password === event.target.password.value)) {
-        window.location.href = "./Index.html"; 
+    if ((user.email  === event.target.email.value) && (user.password === event.target.pw.value)) {
         localStorage.setItem("isLogin", true); 
+        window.location.href = "./Index.html"; 
+       
     }
     else 
         alert("Ban da nhap sai."); 
